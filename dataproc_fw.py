2c957c787b50f4ee2494cf4590b7268256874adb
Angel <angel_sabillon93@yahoo.es>
2024-08-02 18:28:47 +0900

    more files

--- dataproc_fw.py
+++ dataproc_fw.py
@@ -37,38 +37,6 @@ savedir = r'C:\\Users\\hudso\\Downloads\\HudsonAI\\subsets\\'
 
 
 def load_ds(subset):
-    # #For Google Collab
-    # import os, sys
-    # from google.colab import drive
-    # drive.mount('/content/drive')
-    # nb_path = '/content/notebooks'
-    # os.symlink('/content/drive/My Drive/Colab Notebooks', nb_path)
-    # sys.path.insert(0, nb_path)
-
-    # !pip install --target=$nb_path git+https://github.com/huggingface/transformers.git accelerate datasets[audio]
-
-    # !pip install --target=$nb_path datasets
-
-    # pip install git+https://github.com/huggingface/transformers.git accelerate datasets[audio]
-
-    # pip install datasets
-
-    # FOR EXECUTING ALL THE FILES AND GENERATING MANY CSVs with filtered rows
-    '''
-
-    LOAD MODEL (function)
-    First execute a for loop with all the shards/subsets (function)
-    For each one, LOAD DATA and execute the pipeline
-    Generate csv
-    decide 
-
-
-    '''
-
-    # all folders in espnet/yodas (224 subsets)
-    # done 'aa000', 'ab000', 'af000', 'ak000', 'am000', 'ar000', 'as000', 'ay000', 'az000', 'ba000', 'be000', 'bg000'
-    # 'bh000', 'bi000', 'bm000', 'bn000', 'bo000', 'br000', bs000',
-    
     # configs = ['aa000', 'ab000', 'af000', 'ak000', 'am000', 'ar000', 'as000', 'ay000', 'az000', 'ba000', 'be000', 'bg000',
     # 'bh000', 'bi000', 'bm000', 'bn000', 'bo000', 'br000', 'bs000', 'ca000', 'co000', 'cr000', 'cs000', 'cy000', 
     # 'da000', 'de000', 'de100', 'de101', 'de102', 'dz000', 'ee000', 'el000', 'en000', 'en001', 'en002', 'en003', 'en004', 
@@ -87,37 +55,19 @@ def load_ds(subset):
     # 'sv000', 'sw000', 'ta000', 'te000', 'tg000', 'th000', 'th100', 'ti000', 'tk000', 'tn000', 'to000', 'tr000', 'tr100', 
     # 'ts000', 'tt000', 'ug000', 'uk000', 'uk100', 'ur000', 'uz000', 've000', 'vi000', 'vi100', 'vi101', 'vo000', 'wo000', 
     # 'xh000', 'yi000', 'yo000', 'zh000', 'zu000']
-    # configs = ['xh000']
 
-    # for subset in configs:
     while True:
         try:
             ds = load_dataset('espnet/yodas', subset, split="train", streaming=True, trust_remote_code=True, )
             print('Read this >>>' , subset)
-            print(ds)
+            # print(ds)
             making_transcription(subset, ds, lm)
             # this STREAMING loading will finish quickly, can load only one by one
-            #subset = 'mk000'
         except Exception as e: 
             print(e)
             continue
         else:
             break
-
-
-# def load_ds(lm, subset):
-#     while True:
-#         try:
-#             ds = load_dataset('espnet/yodas', subset, split="train", streaming=True, trust_remote_code=True)
-#             print(ds)
-#             print('Read this:', subset)
-#             making_transcription(subset, ds, lm)
-#         except Exception as e:
-#             print(e)
-#             continue
-#         else:
-#             print('haremos break')
-#             break
         
 def load_model():
     model_size = "large-v3"
@@ -129,44 +79,26 @@ def load_model():
     # model = WhisperModel(model_size, device="cuda", compute_type="int8_float16")
     # or run on CPU with INT8
     # model = WhisperModel(model_size, device="cpu", compute_type="int8")
-
     return model
 lm = load_model()
-
-
-# change array to audiofile-like
-# def array_to_filelike(audio_array, sampling_rate):
-#     filelike = io.BytesIO()
-#     sf.write(filelike, audio_array, sampling_rate, format='WAV')
-#     filelike.seek(0)
-#     return filelike
 
 def preprocess_text(text):
     return text.strip().lower().translate(str.maketrans('', '', string.punctuation))
 
 def making_transcription(subset, ds, model):
-    language = subset[0:2] if subset[0:2] in languages else None
-
     # Configure logging
     logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
-
-    # def evaluate_transcriptions(sample, pipe):
-    # if len(results_dict) == 0: 
+    language = subset[0:2] if subset[0:2] in languages else None
     correct_transcriptions = 0
     total = 0
     transcribed_text = ''     #needed if no segments come
-    # results_ls = []
-    # results_dict = []
     results_ls_filtered = []
     metadatas = []
 
     for i in tqdm(ds, desc=f"Processing {subset} Audio Samples"):
         # audio_dict = dict(i['audio']) #somehow i['audio'] gets empty {} after used in pipeline
         audio_array = i['audio']['array']
-        # audio_array = audio_info['array']
-        # sampling_rate = audio_info['sampling_rate']
 
-        # audio_filelike = array_to_filelike(audio_array, sampling_rate)
         with torch.no_grad():
             try:
                 segments, _ = model.transcribe(audio_array, beam_size=1, language=language)
@@ -181,55 +113,27 @@ def making_transcription(subset, ds, model):
 
             transcribed_text = preprocess_text(transcribed_text)
 
-            # ratio = SequenceMatcher(None, expected_text, transcribed_text).ratio()
-
             if transcribed_text == expected_text: # if texts match!
                 # matched = 1
                 correct_transcriptions += 1
-                
-                # if i['id'] not in results_ls_filtered:
+            
                 results_ls_filtered.append(i['id'])
                 save_temp(subset, results_ls_filtered)
-                # else: 
-                #     break
-                # Originally dataset keeps reading after it ends, for big subsets, 
-                # with this rule, when it finds the same id (repeating), will break and go to the next
-                # metadatas.append({'id': i['id'], 'utt_id': i['utt_id'], 
-                #                 'expected': i['text'], 'transcribed': transcribed_text, 'audio': audio_dict})
-            # else:
-            #     matched = 0
-            # print(i['id'])
-            # if i['id'] not in results_ls_filtered:
-            #     print('guarda file?')
-            #     save_temp(subset, results_ls_filtered)
-            # else:
-            #     print('ya estaba y rompe, pasa a siguiente', subset)
-            #     break
             
             accuracy = correct_transcriptions / total
-            # results_ls.append([i['id'], i['text'], transcribed_text, accuracy, ratio])
-            # results_dict.append({'id': i['id'], 'exptext': i['text'], 'tratext': transcribed_text, 'acc': accuracy, 'ratio': ratio})
 
             # logging.info(f"Processed {total} samples. Current accuracy: {accuracy:.2%}")
             torch.cuda.empty_cache()
             # gc.collect()
             # break #just test all subsets
+            # state_dict = ds.state_dict()
+            
     final_accuracy = correct_transcriptions / total
     # logging.info(f"Final Accuracy: {final_accuracy:.2%}")
     # metadatas_ds = Dataset.from_list(metadatas)
 
     # If needed, print the final accuracy
     # print(f"Final Accuracy: {final_accuracy:.2%}")
-
-    # for i in results_ls:
-    #     print(i)
-    #     print(len(results_ls))
-    #     break
-
-    # for i in metadata:
-    #     print(i)
-    #     print(len(metadata))
-    #     break
 
     save_csv(subset, results_ls_filtered, correct_transcriptions, total, final_accuracy)
     # save_ds(subset, metadatas_ds) # SAVE DICTIONARY TO DISK, REQUIRES SPACE
@@ -240,7 +144,6 @@ def save_ds(subset, metadatas_ds):
 def save_csv(subset, results_ls_filtered, ct, tot, acc):
     # Save IDs to csv file to read later
     # import csv
-
     with open(savedir + subset + '_'+ str(ct) + '_'+ str(tot) + '_'+ str(round(acc,2)) + '.txt', 'w') as f:
 
         # using csv.writer method from CSV package
@@ -337,22 +240,32 @@ if __name__ == '__main__':
     #     'en125', 'en126', 'en127', 'eo000', 'es000', 'es100', 'es101', 'es102', 'es103', 'es104', 'es105', 'es106', 'et000', 
     #     'eu000']
 
-    configs = ["as000",
-"rw000",
-"br000",
-"bo000",
-"qu000",
-"af000",
-"am000",
-"ps000"]
+    # configs = ['km000','si000','mr000',
+    #       'gl000','eo000','ml000','ky000','et000','sr000','lt000','eu000','ur000','sl000','hr000','ka000','bn000']
+
+    configs = ['en100',
+'es102',
+'es104',
+'es105',
+# 'es103',
+# 'en108',
+'en126',
+'en107',
+'en119',
+'en106',
+'es101',
+'en122',
+'en121',
+'en120']
 
     # configs = ['es102', 'es103', 'es104', 'es105', 'es106'] 'da000', 'de000', 'de100', 
     # 'en110' --- 140K??? 
     # 'en111',
     #  'ff000', 'fj000','fo000','fy000', 'ga000', 
     #     'gd000','gn000', 'gu000', 'ru001'
-
-    
+    #  'rw000', 'sa000', 'sc000', 
+        # 'sd000','sn000', 'so000', 'st000', 'su000','tg000',  'ti000', 'tk000', 'tn000', 'tt000', 'ug000', 'uz000', 've000',
+        # 'vi101', --- problems
 
     #     # Execute in parallel
     # memory = torch.cuda.mem_get_info(device='cuda:0')[0]//1024 ** 2
@@ -375,7 +288,7 @@ if __name__ == '__main__':
     # pool_obj = mp.Pool(2)
     # pool_obj.map(load_ds, configs)
 
-    # with get_context("spawn").Pool(processes=1) as pool:
+    # with get_context("spawn").Pool(processes=2) as pool:
     #     pool.map(load_ds, configs)
 
     for config in configs:
