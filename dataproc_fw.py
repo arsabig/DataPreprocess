8d6543e2dfe7197ddb848cf44c199be9d1368133
Angel <angel_sabillon93@yahoo.es>
2024-09-06 16:21:12 +0900

    distil whisper and whister-commented

--- dataproc_fw.py
+++ dataproc_fw.py
@@ -67,12 +67,13 @@ def load_ds(subset):
         
 def load_model():
     device = "cuda:0" if torch.cuda.is_available() else "cpu"
-    torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
+    torch_dtype = torch.float16
 
-    model_id = "openai/whisper-large-v3"
+    model_id = "distil-whisper/distil-large-v3"
+    # model_id = "openai/whisper-large-v3"
 
     model = AutoModelForSpeechSeq2Seq.from_pretrained(
-        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
+        model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, attn_implementation="sdpa", use_safetensors=True
     )
     model.to(device)
 
